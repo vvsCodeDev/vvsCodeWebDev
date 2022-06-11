77fbb5897730fd04181f45ff36b3211fdc7833ea
Justin <jcalhoun@happyfuncorp.com>
2022-06-11 13:44:55 -0700

    test config

--- public/app.js
+++ public/app.js
@@ -1,18 +1,26 @@
 // import 'dotenv/config';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+//require('dotenv').config; //for node on backend
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDERID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
 
-firebse.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDERID,
+//     appId: process.env.APP_ID,
+//     measurementId: process.env.MEASUREMENT_ID
+// };
+
+// firebse.initializeApp(firebaseConfig);
+
+
+const functions = require('firebase-functions');
+
+const config =  functions.config();
+const firebaseConfig = config.firebase;
 
 //Reference contactInfo collections
 let contactInfo = firebase.database().ref('info');
