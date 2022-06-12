fa9fa2f8161821518e735b5d193a35b677f1427a
Justin <jcalhoun@happyfuncorp.com>
2022-06-12 09:48:53 -0700

    test

--- public/app.js
+++ public/app.js
@@ -1,25 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDcSNvPWRrz3uxqandnEeIfG8NzyEd6NMA",
-  authDomain: "vcwi-115c6.firebaseapp.com",
-  databaseURL: "https://vcwi-115c6-default-rtdb.firebaseio.com",
-  projectId: "vcwi-115c6",
-  storageBucket: "vcwi-115c6.appspot.com",
-  messagingSenderId: "625669501307",
-  appId: "1:625669501307:web:0eb55bf4cb881610b20dde",
-  measurementId: "G-H6M403YJWG"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 //Reference contactInfo collections
 let contactInfo = firebase.database().ref('info');
